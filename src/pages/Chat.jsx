d76a92612ae387b56928ca37c73709135e42589c
WajeehaHashmi27 <wajeehahashmi85@gmail.com>
2023-12-17 20:15:49 +0500

    commit

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import ForumOutlinedIcon from "@mui/icons-material/ForumOutlined";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+export default function Chat() {
+  return (
+    <div className="relative z-0 flex h-screen w-screen overflow:hidden bg-neutral-100">
+      <div className="relative flex h-full max-w-full flex-1 flex-col overflow:hidden bg-green-100">
+        chat area
+      </div>
+      <div
+        className=" h-screen flex flex-col flex-shrink-0 overflow-x-hidden bg-green-300"
+        style={{ width: "359px" }}
+      >
+        <div
+          className="relative p-2 mt-6 mb-2 max-w-sm mx-auto bg-white rounded-3xl shadow-lg flex items-center"
+          style={{ width: "300px", height: "40px" }}
+        >
+          <div className="relative flex items-center rounded-full bg-stone-200">
+            <span className="pb-1 px-1 text-neutral-900">
+              <PersonOutlineOutlinedIcon
+                className=""
+                style={{ fontSize: "20px" }}
+              />
+            </span>
+          </div>
+          <span
+            className="flex flex-nowrap pl-2 text-sm overflow-hidden whitespace-nowrap font-bold text-neutral-800"
+            style={{ width: "200px" }}
+          >
+            Wajeeha Hashmi
+          </span>
+          <span
+            className="relative flex items-center rounded-full bg-stone-200"
+            style={{ padding: "2px" }}
+          >
+            <NotificationsNoneOutlinedIcon
+              className="px-1 text-neutral-900"
+              style={{ fontSize: "26px" }}
+            />
+          </span>
+          <span
+            className="relative flex items-center rounded-full ml-1 bg-stone-200"
+            style={{ padding: "4px" }}
+          >
+            <SettingsOutlinedIcon
+              className="px-1 text-neutral-900"
+              style={{ fontSize: "22px" }}
+            />
+          </span>
+        </div>
+        <div
+          className="relative p-2 mx-7 mt-2 mb-0 bg-white shadow-lg flex items-center"
+          style={{ width: "300 px", height: "77vh", borderRadius: "40px" }}
+        ></div>
+
+        <div
+          className="relative p-2 mt-4 mb-2 max-w-sm mx-auto bg-white rounded-3xl shadow-lg flex items-center"
+          style={{ width: "300px", height: "40px" }}
+        ></div>
+      </div>
+    </div>
+  );
+}
